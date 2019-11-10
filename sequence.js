b01c030da40c32fb6d7a452a8a49e8bb1104a2ae
Oleg Galansky <oleg140329@gmail.com>
2019-11-10 14:47:43 +0000

    done first

---
+++ sequence.js
@@ -0,0 +1,24 @@
+
+function sequence(number = 0, step = 1) {
+  let chek = 0;
+
+  return function generator(){
+    if (chek === 0) {
+      chek++;
+      return number;
+    }
+    return number += step 
+  }
+}
+
+var generator = sequence(10, 3);
+var generator2 = sequence(7, 1);
+
+console.log(generator()); // 10
+console.log(generator()); // 13
+
+console.log(generator2()); // 7
+
+console.log(generator()); // 16
+
+console.log(generator2()); // 8
